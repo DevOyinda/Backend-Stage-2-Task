fe1e5446b02b2641c20eb09b18151656238f691a
DevOyinda <kudiratoyindamolad@gmail.com>
2022-11-05 22:17:42 +0100

    Add project files.

---
+++ Backend Stage 2 Task/Controllers/Stage2Controller.cs
@@ -0,0 +1,57 @@
+﻿using Backend_Stage_2_Task.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Stage_2_Task.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Stage2Controller : Controller
+    {       
+        [HttpPost]
+        [Route("PostCalculation")]
+        public IActionResult PostCalculation([FromQuery]Input input)
+        {
+            var res = new Response();
+            if (input.operation_type == Oyin.Addition)
+            {
+                res = new Response()
+                {
+                    slackUsername = "DevOyinda",
+                    result = input.x + input.y,
+                    operation_type = input.operation_type.ToString()
+                };
+            }
+
+            else if (input.operation_type == Oyin.Substraction)
+            {
+                res = new Response()
+                {
+                    slackUsername = "DevOyinda",
+                    result = input.x - input.y,
+                    operation_type = input.operation_type.ToString()
+                };
+            }
+
+            else if (input.operation_type == Oyin.Multiplication)
+            {
+                res = new Response()
+                {
+                    slackUsername = "DevOyinda",
+                    result = input.x * input.y,
+                    operation_type = input.operation_type.ToString()
+                };
+            }
+
+            else
+            {
+                return NoContent();
+            }
+
+            return Ok(res);                    
+        }
+    }
+}
---
+++ Backend Stage 2 Task/Model/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Stage_2_Task.Model
+{
+    public class Response
+    {
+        public string slackUsername { get; set; }
+
+        public int result { get; set; }
+
+        public string operation_type { get; set; }
+    }
+
+    public class Input
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public Oyin operation_type { get; set; }
+    }
+
+
+    public enum Oyin
+    {
+        Addition = 1,
+        Substraction,
+        Multiplication
+    }
+}
